bd32c88c871cf7860e5eed6cd17ea173dd0b2bb1
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-09-11 14:08:30 -0300

    feat: add ORM in user

--- src/main/java/com/application/userdept/entities/User.java
+++ src/main/java/com/application/userdept/entities/User.java
@@ -1,10 +1,17 @@
 package com.application.userdept.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String email;
-
+  @ManyToOne
+  @JoinColumn(name = "department_id")
   private Department department;
 
   public User(){}
