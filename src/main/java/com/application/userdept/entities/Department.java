29935dc96021ad7a43d755f4bfca2266788c0797
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-09-11 14:04:23 -0300

    feat: add ORM in department

--- src/main/java/com/application/userdept/entities/Department.java
+++ src/main/java/com/application/userdept/entities/Department.java
@@ -1,6 +1,12 @@
 package com.application.userdept.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_department")
 public class Department {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long ind;
   private String name;
   public Department(){}
